c464f6141c0aa1affa4067cefe2d3d4ac94b23ab
Jihau <johanna.toivanen@metropolia.fi>
2022-04-27 21:55:21 +0300

    Fixing format and order of async functions.

--- js/map.js
+++ js/map.js
@@ -1,4 +1,3 @@
-
 let map = L.map('map').setView([1, 1], 1.5);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/jihau/cl2gazbo0000u16o66jok0xt4/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamloYXUiLCJhIjoiY2wyZzM4MnptMDAybTNlbDVydWd4NG1tNCJ9.xX5DfnTTX30CKCHNJnlJpg', {
@@ -11,41 +10,36 @@ L.tileLayer('https://api.mapbox.com/styles/v1/jihau/cl2gazbo0000u16o66jok0xt4/ti
 }).addTo(map);
 
 
-function addMarker(x, y){
-    L.marker([x, y]).addTo(map).bindPopup("")
+birds().then();
+// TODO: Remove test marker
+//  L.marker([60.17604, 24.9386]).addTo(map);
+
+function addMarker(x, y, name) {
+    L.marker([x, y]).addTo(map).bindPopup(!name ? "" : name);
 }
 
-async function birds(){
+async function birds() {
     let myHeaders = new Headers();
     myHeaders.append("X-eBirdApiToken", "p291e2j3pm2c");
 
     let requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
+        method: 'GET', headers: myHeaders, redirect: 'follow'
     };
 
-    let lat,lng,birdName;
-    function addMarker(x, y, name){
-        L.marker([x, y]).addTo(map).bindPopup(name);
-    }
+    let lat, lng, birdName;
 
-    try{
+    try {
         const api = await fetch("https://api.ebird.org/v2/data/obs/FI/recent", requestOptions);
-        if(!api.ok) throw new Error("Something went wrong");
-        const result = await api.json();
-        for (let i = 0; i < result.length; i++) {
-            lat = result[i].lat;
-            lng = result[i].lng;
-            birdName = result[i].comName;
-            console.log(lat);
-            console.log(lng);
-            addMarker(lat, lng);
-            addMarker(lat, lng, birdName);
+        if (api.ok) {
+            const result = await api.json();
+            for (let i = 0; i < result.length; i++) {
+                lat = result[i].lat;
+                lng = result[i].lng;
+                birdName = result[i].comName;
+                addMarker(lat, lng, birdName);
+            }
         }
-    } catch(error){
-        console.log(error);
+    } catch (error) {
+        throw new Error("Something went wrong");
     }
 }
-birds();
-L.marker([60.17604, 24.9386]).addTo(map);
